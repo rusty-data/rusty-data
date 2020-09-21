12577b6e3c6eea6ddfa57d4e7f4b485ed20fdf0d
titaneric <chenyihuang001@gmail.com>
2020-09-22 01:29:11 +0800

    :penguin: Pseudo code for abstract transmission

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+use clap::{App, Arg};
+use std::path::Path;
+
+pub trait Flow {
+    fn flow(source: Box<dyn Source>, sink: Box<dyn Sink>) {
+        source.from();
+        sink.to();
+    }
+}
+pub trait Source {
+    fn from(&self) -> ();
+}
+
+pub trait Sink {
+    fn to(&self) -> ();
+}
+pub mod fs {
+    use crate::{Sink, Source};
+    use std::path::Path;
+
+    impl Source for Path {
+        fn from(&self) {}
+    }
+    impl Sink for Path {
+        fn to(&self) {}
+    }
+    // impl Source for FS {
+    //     fn sync(from: &Path, to: &Path) -> io::Result<()> {
+    //         fs::copy(from, to)?;
+    //         Ok(())
+    //     }
+    // }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("rusty-data")
+        .version("0.0.1")
+        .author("rusty-data")
+        .about("Dataset splitting supporting multiple data sources")
+        .arg(
+            Arg::with_name("file")
+                .help("Sets the input folder to use")
+                .required(true),
+        )
+        .get_matches();
+
+    let folder = Path::new(matches.value_of("file").unwrap_or(""));
+
+    println!("{:?}", folder);
 }
