aa88a97101978198d6179df81e36ed3d0239c13c
shiftdevy <shiftkeydeveloper@gmail.com>
2020-09-21 12:13:48 +0300

    Move function base.
    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::env::args;
+
+fn main() -> std::io::Result<()> {
+    let args_list: Vec<String> = args().collect();
+    let file_one = &args_list[1];
+    let file_two = &args_list[2];
+    let dist = file_two.clone() + &file_one.clone();
+    println!("{}", dist);
+
+    fs::rename(file_one, dist)?;
+    Ok(())
 }
