32971dde4502be01d648a73668dadd3bd763db0d
aschi2 <austinchi2@yahoo.com>
2020-09-21 18:34:02 -0700

    refactored move into seperate function and added some test folders to play with

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,19 @@ use std::env;
 use std::fs;
 
 
-fn main() -> std::io::Result<()> {
+fn main(){
     let args: Vec<String> = env::args().collect();
     let input = &args[1];
     let output = &args[2];
+    file_move(input,output);
     
+
+
+}
+
+
+
+fn file_move(input:&String,output:&String){
     let clone_in = input.clone();
     let heap: Vec<&str> = clone_in.split("/").collect();
     println!("{:?}", heap);
@@ -17,7 +25,10 @@ fn main() -> std::io::Result<()> {
 
     let target = output.clone() + "/" + part.clone();
 
-    fs::rename(input, target)?;
+    fs::rename(input, target).expect("failed to move");
+    
+    
 
-    Ok(())
+
 }
+
